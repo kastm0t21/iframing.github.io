c47eae100e830fd0df125d8cb178b90f54f02ad8
kastm0t21 <kastm0t21@gmail.com>
2020-01-23 18:59:16 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+        <title>Suuvern</title>  <!-- Change "Domain Name" to website domain -->
+        
+        <style type="text/css">
+            body, html
+            {
+                margin: 0; padding: 0; height: 100%; overflow: hidden;
+            }
+
+            #content
+            {
+                position:absolute; left: 0; right: 0; bottom: 0; top: 0px; 
+            }
+            div
+            {
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="content">
+            <iframe width="100%" height="100%" frameborder="0" src="https://www.floretflowers.com/blog/" />
+        </div>
+    </body>
+</html>
